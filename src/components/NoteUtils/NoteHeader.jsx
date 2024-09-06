12b82b38f45f9469d50f428b23ac51324696ebdd
Andra R. Pratama <82736568+nulitas@users.noreply.github.com>
2024-09-06 22:34:13 +0700

    Update NoteHeader.jsx

--- src/components/NoteUtils/NoteHeader.jsx
+++ src/components/NoteUtils/NoteHeader.jsx
@@ -2,7 +2,7 @@ import NoteSearch from "./NoteSearch";
 export default function NoteHeader({ search, onSearch }) {
   return (
     <div className="note-app__header">
-      <h1>notedx.</h1>
+      <h1>note-app</h1>
       <NoteSearch search={search} onSearch={onSearch} />
     </div>
   );
